4063ceac8ac38274995e4b39a28e5af79c47c817
Nguyễn Việt <vietndhe181427@fpt.edu.vn>
2026-03-09 23:07:29 +0700

    add be room
    add controller and routes about room and connect serverjs

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ import reviewRoutes from "./routes/review.routes.js";
 
 // ===== [NGUOI 3] Theaters, Rooms & Seats =====
 import theaterRoutes from "./routes/theater.routes.js";
-// import roomRoutes from "./routes/room.routes.js";
+import roomRoutes from "./routes/room.routes.js";
 // import seatRoutes from "./routes/seat.routes.js";
 // import publicRoutes from "./routes/public/public.routes.js";
 // ===== END [NGUOI 3] =====
@@ -75,7 +75,7 @@ app.use("/api/reviews", reviewRoutes);
 // ===== [NGUOI 3] Theater, Room & Seat Routes =====
 // app.use("/api/public", publicRoutes);
 app.use("/api/theaters", theaterRoutes);
-// app.use("/api/rooms", roomRoutes);
+app.use("/api/rooms", roomRoutes);
 // app.use("/api/seats", seatRoutes);
 // ===== END [NGUOI 3] =====
 
